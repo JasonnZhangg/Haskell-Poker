a3ae4b668cd6178ac615561d39f92a2578f254d5
Daniel P <46207543+danielPeng-17@users.noreply.github.com>
2020-03-20 21:31:09 -0400

    finish pair and 2 pair

--- Poker.hs
+++ Poker.hs
@@ -1,103 +1,72 @@
 module Poker where 
-	import Data.List
-	
-	deal cards = do
-	--Seperate the hands 
-		let handOne = handOnef cards
-		let handTwo = handTwof cards
-		
-		let handOneP = calculatePoints handOne
-		let handTwoP = calculatePoints handTwo
-		
-		comparePoints handOneP handTwoP
-	
-	
-	--Compare points
-	comparePoints handO handT = do	
-		if head handO < head handT then "handO win"
-		else if head handO > head handT then "handT win"
-		else 
-			if (handO !! 1) > (handT !! 1) then "handO win"
-			else if (handO !! 1) < (handT !! 1) then "handT win"
-			else 
-				if (handO !! 2) > (handT !! 2) then "handO win"
-				else if (handO !! 2) < (handT !! 2) then "handT win"
-				else "tie"
-		
-	
-	--Function to calculate points for each hand 
-		--The lower the points, the better the hand 
-	calculatePoints lst = do
-		if isRoyalFlush lst then 
-			if allSpades lst then [1,1,1]
-			else if allHearts lst then [1,1,2]
-			else if allDiamonds lst then [1,1,3]
-			else if allClubs lst then [1,1,4]
-			else [11,0,0] 
-			
-		--Checks for Straight Flush	
-		else if isStraight lst then
-			if allSpades lst then [2, findHighestS lst, 1]
-			else if allHearts lst then [2, findHighestS lst, 2]
-			else if allDiamonds lst then [2, findHighestS lst, 3]
-			else if allClubs lst then [2, findHighestS lst, 4]
-			else [11,0,0]
-		
-		--Checks for Straight
-		else if isStraight lst then
-			if isSpades (findHighestS lst) then [6, findHighestS lst, 1]
-			else if isHearts (findHighestS lst) then [6, findHighestS lst, 2]
-			else if isDiamonds (findHighestS lst) then [6, findHighestS lst, 3]
-			else if isClubs (findHighestS lst) then [6, findHighestS lst, 4]
-			else [11,0,0]
-			
-		--Checks for Highest Card
-		else 
-			if isSpades (findHighestS lst) then [10, findHighestS lst, 1]
-			else if isHearts (findHighestS lst) then [10, findHighestS lst, 2]
-			else if isDiamonds (findHighestS lst) then [10, findHighestS lst, 3]
-			else if isClubs (findHighestS lst) then [10, findHighestS lst, 4]
-			else [11,0,0] 
-		
-	
-	--Checks if it has a royal flush
-	isRoyalFlush lst = do
-		let temp = map (`mod` 13) lst 
-		if all (\x -> x == 1|| x == 10 || x == 11 || x == 12 || x == 0) temp then True
-		else False
-		
-	--Checks if hand is straight	
-	isStraight lst	= do
-		let temp = sort (map (`mod` 13) lst)
-		let zipped = zip [head temp..] temp
-		if all(\x -> fst x == snd x) zipped then True
-		else False
-	
-	--Finds the highest card in the hand
-	findHighestS lst = do
-		let temp = map (`mod` 13) lst 
-		maximum temp
-		
-	--Checks if the hand has same suit
-	allClubs lst = all (\x -> isClubs x) lst
-	allDiamonds lst = all (\x -> isDiamonds x) lst
-	allHearts lst = all(\x -> isHearts x) lst
-	allSpades lst = all(\x -> isSpades x) lst
-	
-	--Checks if card is certain suit
-	isClubs x = 
-		if(x>0 && x<14) then True
-		else False 	
-	isDiamonds x = 
-		if(13<x && x<27) then True
-		else False 
-	isHearts x = 
-		if(26<x && x<40) then True
-		else False 
-	isSpades x = 
-		if(39<x && x<53) then True
-		else False 		
-	
-	--Hand one become every odd nth card while Hand two is every even nth card
-	handOnef lst = [snd x | x <- (zip [1..10] lst), odd(fst x)]
-	handTwof lst = [snd x | x <- (zip [1..10] lst), even(fst x)]
+    import Data.List
+    
+    deal deck = do
+        let handOne = [snd card | card <- (zip [1..10] deck), odd(fst card)]
+        let handTwo = [snd card | card <- (zip [1..10] deck), even(fst card)]
+        handTwo
+    
+    -- mod the whole hand
+    modHand [] = []
+    modHand (x:xs) = do
+        if (mod x 13 == 0) then
+            13:modHand xs
+        else
+            (mod x 13):modHand xs
+    
+    -- mod a single card 
+    reduceSingleCard card = do 
+        if (mod card 13) == 0 then
+            13
+        else 
+            mod card 13
+
+    isPair hand = do 
+        -- find pair
+        let pairValue = findPair hand (-1)
+        -- find the biggest value of the pair
+        let highCard = filter (\x -> reduceSingleCard x == pairValue) hand
+        -- list of all the non pair cards
+        let nonPairCards = filter (\x -> reduceSingleCard x /= pairValue) hand
+        if (pairValue /= (-1)) then
+            -- [ score, biggest pair value, suit of the pair ], [ list of non pair cards ]
+            [[9, pairValue, suitChecker (maximum highCard)], nonPairCards]
+        else 
+            [[11]]
+    
+    isTwoPair hand = do
+        -- find the 2 pairs 
+        let firstPairValue = findPair hand (-1)
+        let secondPairValue = findPair hand firstPairValue
+        -- find the biggest pair 
+        let maxPair = maximum [firstPairValue, secondPairValue]
+        -- get the highest suit card of the biggest pair
+        let highCard = filter (\x -> reduceSingleCard x == maxPair) hand
+        -- get all of the non pair cards
+        let nonPairCards = filter (\x -> reduceSingleCard x /= maxPair) hand
+        if (firstPairValue /= (-1) && secondPairValue /= (-1)) then
+            -- [ score, biggest pair, smallest pair, suit of the biggest pair ], [ list of non pair cards ]
+            [[8, maxPair, minimum [firstPairValue, secondPairValue], suitChecker (maximum highCard)], nonPairCards]
+        else 
+            [[11]]
+
+    findPair hand foundNum = do 
+        let temp = modHand hand
+        fpair temp foundNum
+
+    fpair [] _ = -1
+    fpair (element:last) foundNum = do
+        if (element /= foundNum) && (elem element last) then
+            element
+        else 
+            fpair last foundNum
+
+    suitChecker x = do
+        if (x > 0 && x < 14) then 
+            4
+        else if (x > 13 && x < 27) then
+            3
+        else if (x > 26 && x < 40) then
+            2
+        else
+            1
